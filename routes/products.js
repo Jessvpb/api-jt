e5ad2a8e4a6c59f34a872d8eee4052e670c39762
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2025-01-14 12:16:23 +0700

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-const ProductController = require("../controller/product");
+const ProductController = require("../../controller/product");
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond dari CATEGORY router');
