2a1550ea29186c365333bd1d079f3ba7f7595591
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2025-01-14 11:51:27 +0700

    .

---
+++ app_server/routes/brands.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var router = express.Router();
+//const checkAuth = require("../middleware/check-auth");
+const BrandController = require("../controller/brand");
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond dari CATEGORY router');
+// });
+
+//insert
+router.post("/", BrandController.createBrand);
+
+//select
+router.get("/", BrandController.readBrand);
+
+//delete
+router.delete("/:kdBrand", BrandController.deleteBrand);
+
+//update
+router.put("/:kdBrand", BrandController.updateBrand);
+
+module.exports = router;
+
+// var express = require("express");
+// var router = express.Router();
+// const checkAuth = require("../middleware/check-auth");
+// const BrandController = require("../controller/brand");
+// /* GET users listing. */
+// // router.get('/', function(req, res, next) {
+// //   res.send('respond dari CATEGORY router');
+// // });
+
+// //insert
+// router.post("/", checkAuth, BrandController.createBrand);
+
+// //select
+// router.get("/", checkAuth, BrandController.readBrand);
+
+// //delete
+// router.delete("/:kdBrand", checkAuth, BrandController.deleteBrand);
+
+// //update
+// router.put("/:kdBrand", checkAuth, BrandController.updateBrand);
+
+// module.exports = router;
---
+++ app_server/routes/products.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var router = express.Router();
+//const checkAuth = require("../middleware/check-auth");
+const ProductController = require("../controller/product");
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond dari CATEGORY router');
+// });
+
+//insert
+router.post("/", ProductController.createProduct);
+
+//select
+router.get("/", ProductController.readProduct);
+
+//delete
+router.delete("/:kdProduct", ProductController.deleteProduct);
+
+//update
+router.put("/:kdProduct", ProductController.updateProduct);
+
+module.exports = router;
+// var express = require("express");
+// var router = express.Router();
+// const checkAuth = require("../middleware/check-auth");
+// const ProductController = require("../controller/product");
+// /* GET users listing. */
+// // router.get('/', function(req, res, next) {
+// //   res.send('respond dari CATEGORY router');
+// // });
+
+// //insert
+// router.post("/", checkAuth, ProductController.createProduct);
+
+// //select
+// router.get("/", checkAuth, ProductController.readProduct);
+
+// //delete
+// router.delete("/:kdProduct", checkAuth, ProductController.deleteProduct);
+
+// //update
+// router.put("/:kdProduct", checkAuth, ProductController.updateProduct);
+
+// module.exports = router;
