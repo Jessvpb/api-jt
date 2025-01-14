4f87adc957d383db6e2789dbc12cc841e4edb157
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2025-01-14 14:47:42 +0700

    Update user.js

--- app_server/model/user.js
+++ app_server/model/user.js
@@ -4,8 +4,6 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ["admin", "user"] },
-  created_at: { type: Date, default: Date.now },
 });
 
 userSchema.plugin(uniqueValidator);
