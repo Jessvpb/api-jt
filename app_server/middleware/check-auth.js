a10e0c42b91bfe8addfe1a9016bf5edeb75ce72e
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2025-01-16 21:15:18 +0700

    Update check-auth.js

--- app_server/middleware/check-auth.js
+++ app_server/middleware/check-auth.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "kunciLCI");
+    jwt.verify(token, "kuncisi5bpaw");
     next();
-    //req.userData = { email: decodedToken.email, userid: decodedToken.userid };
+    // req.userData = { email: decodedToken.email, userid: decodedToken.userid };
   } catch (error) {
     res.status(401).json({ message: "Auth failed!" });
   }
