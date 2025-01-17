5245bd9653bb4da404a6d6668df5be40234675ec
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2025-01-17 22:59:52 +0700

    Update brand.js

--- app_server/controller/brand.js
+++ app_server/controller/brand.js
@@ -27,7 +27,7 @@ const createBrand = (req, res) => {
     .catch((err) => {
       res.status(500).json({
         message: "Gagal menyimpan brand",
-        error: err.message,
+        //error: err.message,
       });
     });
 };
